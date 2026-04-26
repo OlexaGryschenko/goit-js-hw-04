d1c4b237b8c2cc76075addd0cb70e7bbed6a4ec3
WIN-QUG5H71A7UD <baraban777771962@gmail.com>
2026-04-26 23:46:55 +0100

    third

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,24 @@
-function getElementWidth(content, padding, border) {
-  const indexNumContent = content.indexOf('px');
-  const indexNumPadding = padding.indexOf('px');
-  const indexNumBorder = border.indexOf('px');
+const profile = {
+    username: "Jacob",
+    playTime: 300,
 
-  const numContent = content.slice(0, indexNumContent);
+  changeUsername(newName) {
+    profile.username = newName;
+  },
 
-  const numPadding = padding.slice(0, indexNumPadding);
+ updatePlayTime(hours){
+profile.playTime += hours;
+},
 
-  const numBorder = border.slice(0, indexNumBorder);
+  getInfo(){    
+   return `${profile.username} has ${(profile.playTime)} active hours!`
+  }
+};
 
-  return Number(numContent) + Number(numPadding) * 2 + Number(numBorder) * 2;
-}
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
